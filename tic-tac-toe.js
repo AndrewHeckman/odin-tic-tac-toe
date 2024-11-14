e72c5e7a5b45afba36aa33af2a53af72f6ad11cc
Andrew Heckman <93686608+AndrewHeckman@users.noreply.github.com>
2024-11-14 14:44:02 -0600

    Move checkWinner from gameboard to gameController
    Add getSquare function to gameboard to allow gameController
    to see the board

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -8,75 +8,21 @@ const gameboard = (function () {
    * Place a symbol in grid square on gameboard
    * @param {String} symbol Symbol to be placed
    * @param {Number} square Grid square
-   * @return {Boolean} True if symbol successfully placed
+   * @returns {Boolean} True if symbol successfully placed
    */
   function place(symbol, square) {
     if (board[square] != undefined) return false;
     board[square] = symbol;
-    return true; 
+    return true;
   }
 
   /**
-   * Check for a winner
-   * @param {Number} square Square last placed
-   * @return {Array<Number>} Array of winning squares, empty if no winner
+   * Gets the symbol placed in a square
+   * @param {Number} square Square to get symbol of
+   * @returns {String} Symbol in square
    */
-  function checkWinner(square) {
-    const symbol = board[square];
-
-    switch (square) {
-      case 0:{
-        if (board[1] === symbol && board[2] === symbol) return [0, 1, 2];
-        if (board[3] === symbol && board[6] === symbol) return [0, 3, 6];
-        if (board[4] === symbol && board[8] === symbol) return [0, 4, 8];
-        return [];
-      }
-      case 1:{
-        if (board[0] === symbol && board[2] === symbol) return [0, 1, 2];
-        if (board[4] === symbol && board[7] === symbol) return [1, 4, 7];
-        return [];
-      }
-      case 2:{
-        if (board[0] === symbol && board[1] === symbol) return [0, 1, 2];
-        if (board[4] === symbol && board[6] === symbol) return [2, 4, 6];
-        if (board[5] === symbol && board[8] === symbol) return [2, 5, 8];
-        return [];
-      }
-      case 3:{
-        if (board[0] === symbol && board[6] === symbol) return [0, 3, 6];
-        if (board[4] === symbol && board[5] === symbol) return [3, 4, 5];
-        return [];
-      }
-      case 4:{
-        if (board[0] === symbol && board[8] === symbol) return [0, 4, 8];
-        if (board[1] === symbol && board[7] === symbol) return [1, 4, 7];
-        if (board[2] === symbol && board[6] === symbol) return [2, 4, 6];
-        if (board[3] === symbol && board[5] === symbol) return [3, 4, 5];
-        return [];
-      }
-      case 5:{
-        if (board[2] === symbol && board[8] === symbol) return [2, 5, 8];
-        if (board[3] === symbol && board[4] === symbol) return [3, 4, 5];
-        return [];
-      }
-      case 6:{
-        if (board[0] === symbol && board[3] === symbol) return [0, 3, 6];
-        if (board[2] === symbol && board[4] === symbol) return [2, 4, 6];
-        if (board[7] === symbol && board[8] === symbol) return [6, 7, 8];
-        return [];
-      }
-      case 7:{
-        if (board[1] === symbol && board[4] === symbol) return [1, 4, 7];
-        if (board[6] === symbol && board[8] === symbol) return [6, 7, 8];
-        return [];
-      }
-      case 8:{
-        if (board[0] === symbol && board[4] === symbol) return [0, 4, 8];
-        if (board[2] === symbol && board[5] === symbol) return [2, 5, 8];
-        if (board[6] === symbol && board[7] === symbol) return [6, 7, 8];
-        return [];
-      }
-    }
+  function getSquare(square) {
+    return board[square];
   }
 
   /**
@@ -87,8 +33,8 @@ const gameboard = (function () {
       board[i] = undefined;
     }
   }
-  
-  return {place, checkWinner, clear};
+
+  return { place, getSquare, clear };
 })();
 
 /**
@@ -98,6 +44,76 @@ const displayController = (function () {
 
 })();
 
-function playerFactory () {
+/**
+ * Controls the logic of how the game is played
+ */
+const gameController = (function () {
+  /**
+     * Check for a winner
+     * @param {Number} square Square last placed
+     * @return {Array<Number>} Array of winning squares, empty if no winner
+     */
+  function checkWinner(square) {
+    const symbol = gameboard.getSquare(square);
+
+    switch (square) {
+      case 0: {
+        if (gameboard.getSquare(1) === symbol && gameboard.getSquare(2) === symbol) return [0, 1, 2];
+        if (gameboard.getSquare(3) === symbol && gameboard.getSquare(6) === symbol) return [0, 3, 6];
+        if (gameboard.getSquare(4) === symbol && gameboard.getSquare(8) === symbol) return [0, 4, 8];
+        return [];
+      }
+      case 1: {
+        if (gameboard.getSquare(0) === symbol && gameboard.getSquare(2) === symbol) return [0, 1, 2];
+        if (gameboard.getSquare(4) === symbol && gameboard.getSquare(7) === symbol) return [1, 4, 7];
+        return [];
+      }
+      case 2: {
+        if (gameboard.getSquare(0) === symbol && gameboard.getSquare(1) === symbol) return [0, 1, 2];
+        if (gameboard.getSquare(4) === symbol && gameboard.getSquare(6) === symbol) return [2, 4, 6];
+        if (gameboard.getSquare(5) === symbol && gameboard.getSquare(8) === symbol) return [2, 5, 8];
+        return [];
+      }
+      case 3: {
+        if (gameboard.getSquare(0) === symbol && gameboard.getSquare(6) === symbol) return [0, 3, 6];
+        if (gameboard.getSquare(4) === symbol && gameboard.getSquare(5) === symbol) return [3, 4, 5];
+        return [];
+      }
+      case 4: {
+        if (gameboard.getSquare(0) === symbol && gameboard.getSquare(8) === symbol) return [0, 4, 8];
+        if (gameboard.getSquare(1) === symbol && gameboard.getSquare(7) === symbol) return [1, 4, 7];
+        if (gameboard.getSquare(2) === symbol && gameboard.getSquare(6) === symbol) return [2, 4, 6];
+        if (gameboard.getSquare(3) === symbol && gameboard.getSquare(5) === symbol) return [3, 4, 5];
+        return [];
+      }
+      case 5: {
+        if (gameboard.getSquare(2) === symbol && gameboard.getSquare(8) === symbol) return [2, 5, 8];
+        if (gameboard.getSquare(3) === symbol && gameboard.getSquare(4) === symbol) return [3, 4, 5];
+        return [];
+      }
+      case 6: {
+        if (gameboard.getSquare(0) === symbol && gameboard.getSquare(3) === symbol) return [0, 3, 6];
+        if (gameboard.getSquare(2) === symbol && gameboard.getSquare(4) === symbol) return [2, 4, 6];
+        if (gameboard.getSquare(7) === symbol && gameboard.getSquare(8) === symbol) return [6, 7, 8];
+        return [];
+      }
+      case 7: {
+        if (gameboard.getSquare(1) === symbol && gameboard.getSquare(4) === symbol) return [1, 4, 7];
+        if (gameboard.getSquare(6) === symbol && gameboard.getSquare(8) === symbol) return [6, 7, 8];
+        return [];
+      }
+      case 8: {
+        if (gameboard.getSquare(0) === symbol && gameboard.getSquare(4) === symbol) return [0, 4, 8];
+        if (gameboard.getSquare(2) === symbol && gameboard.getSquare(5) === symbol) return [2, 5, 8];
+        if (gameboard.getSquare(6) === symbol && gameboard.getSquare(7) === symbol) return [6, 7, 8];
+        return [];
+      }
+    }
+  }
+
+  return {checkWinner}
+})();
+
+function playerFactory() {
 
 }
